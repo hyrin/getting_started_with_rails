42e9bd3d755ca4506106ab8e7159d2fb3baf4abe
hyrin <hyrin@list.ru>
2018-03-04 10:17:24 +0400

    tests

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,7 +1,52 @@
 require 'test_helper'
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @article = articles(:one)
+  end
+
+  test "should get index" do
+    get articles_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_article_url
+    assert_response :success
+  end
+
+  test "should create article" do
+    assert_difference('Article.count') do
+      params = { author_name: @article.author_name, state: @article.state, title: @article.title, text: @article.text }
+      post articles_url, params: { article: params }
+    end
+
+    assert_redirected_to article_url(Article.last)
+  end
+
+  test "should show article" do
+    get articles_url(@article)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_article_url(@article)
+    assert_response :success
+  end
+
+  test "should update article" do
+    params = { author_name: @article.author_name, state: @article.state, title: @article.title, text: @article.text }
+    patch article_url(@article), params: { article: params }
+    assert_redirected_to article_url(@article)
+  end
+
+
+  test "should destroy article" do
+    assert_difference('Article.count', -1) do
+      delete article_url(@article)
+    end
+
+    assert_redirected_to article_url
+  end
+
 end
--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create comment" do
+    assert_difference('Comment.count') do
+      post article_comments_url(@article), params:
+        {
+          comment:
+            {
+              commenter: @comment.commenter,
+              body: @comment.body,
+              article_id: @article.id
+            }
+        }
+    end
+
+    assert_response :redirect
+  end
+
+  test "should destroy comment" do
+    assert_difference('Comment.count', -1) do
+      delete comment_url(@article)
+    end
+
+    assert_redirected_to article_url
+  end
 end
